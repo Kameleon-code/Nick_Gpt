19e16a2dfe57c8375d0b8145e31de7d89d770238
Pizza-code-8 <ggpllagg@gmail.com>
2025-02-02 13:49:14 +0700

    Update luma_udio.py

--- db/luma_udio.py
+++ db/luma_udio.py
@@ -14,51 +14,51 @@ def check_user_in_luma(user_id):
 
 #Добавлене юезра в бд
 def add_user_in_luma(user_id: str, days: int, times: int, mode: str, seconds: str):
-    cur.execute(f"INSERT INTO luma_udio (user_id, days, times, mode, seconds) VALUES (?, ?, ?, ?, ?)", (user_id, days, times, mode, seconds))
+    cur.execute(f"INSERT INTO luma (user_id, days, times, mode, seconds) VALUES (?, ?, ?, ?, ?)", (user_id, days, times, mode, seconds))
     conn.commit()
 
 #Удалить юзера из бд
 def delete_user_from_luma(user_id):
-    cur.execute(f"DELETE FROM luma_udio WHERE user_id = {user_id}")
+    cur.execute(f"DELETE FROM luma WHERE user_id = {user_id}")
     conn.commit()
 
 #Вытащить дни пользователя
 def take_days_from_luma(user_id):
-    cur.execute(f"SELECT days FROM luma_udio WHERE user_id = {user_id}")
+    cur.execute(f"SELECT days FROM luma WHERE user_id = {user_id}")
     days = cur.fetchone()[0]
     return days
 
 #Вытащить user_id
 def take_user_id_from_lume(user_id):
-    cur.execute(f"SELECT days FROM luma_udio WHERE user_id = {user_id}")
+    cur.execute(f"SELECT days FROM luma WHERE user_id = {user_id}")
     user_id = cur.fetchone()[0]
     return user_id
 
 #Вытащить версию нейронки
 def take_mode_kling(user_id):
-    cur.execute(f"SELECT mode FROM luma_udio WHERE user_id = {user_id}")
+    cur.execute(f"SELECT mode FROM luma WHERE user_id = {user_id}")
     mode = cur.fetchone()[0]
     return mode
 
 #Вытщить секунды
 def take_seconds(user_id):
-    cur.execute(f"SELECT seconds FROM luma_udio WHERE user_id = {user_id}")
+    cur.execute(f"SELECT seconds FROM luma WHERE user_id = {user_id}")
     sec = cur.fetchone()[0]
     return sec
 
 #Сократитель дней
 def short_days(user_id):
-    cur.execute(f"UPDATE luma_udio SET days = days - {1} WHERE user_id = {user_id}")
+    cur.execute(f"UPDATE luma SET days = days - {1} WHERE user_id = {user_id}")
     conn.commit()
 
 #Сократитель дней
 def times_killer(user_id):
-    cur.execute(f"UPDATE luma_udio SET times = times - {1} WHERE user_id = {user_id}")
+    cur.execute(f"UPDATE luma SET times = times - {1} WHERE user_id = {user_id}")
     conn.commit()
 
 #Достать всех юзеров из бд
 def take_all_luma_users():
-    cur.execute(f"SELECT user_id FROM luma_udio")
+    cur.execute(f"SELECT user_id FROM luma")
     users = cur.fetchall()
     fin_list = []
     for i in users:
